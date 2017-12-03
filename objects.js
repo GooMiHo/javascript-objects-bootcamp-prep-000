fbace97436f6db829865844dd09ca9a446eac950
Bethany  Walker <bethanymarciawalker@outlook.com>
2017-12-03 18:32:47 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -2,9 +2,18 @@ var playlist = new Object({name: "song"});
 
 function updatePlaylist(playlist, artistName, songTitle){
   return Object.assign({}, playlist, {[artistName]: songTitle})
+<<<<<<< HEAD
 }
 
 function removeFromPlaylist(playlist, artistName) {
   delete playlist[artistName];
   return playlist;
+=======
+
+}
+function removeFromPlaylist(playlist, artistName) {
+  delete playlist.artistName;
+  return playlist;
+
+>>>>>>> 5936e504584a4817fab557eb297a47befd2585cf
 }
